c040adbc69e0665196e7f35f9be8d952f9821084
Bipin Kumar <Bipin.Kumar@SDSBLR.COM>
2021-11-26 13:11:09 +0530

    Final scripts to upload data as a tileset into mapbox

---
+++ upload2mapbox.py
@@ -0,0 +1,47 @@
+from mapbox import Uploader
+import argparse
+import os
+
+"""
+Script to upload mapbox supported data as tilesets
+
+Author: Bipin Kumar
+Company: Laminaar Aviation Infotech India Pvt. Ltd.
+Date: 17 Nov 2021
+"""
+
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        description="Upload mapbox supported data to mapbox"
+    )
+    parser.add_argument("--dir", type=str, help="Directory to upload to mapbox")
+
+    return parser.parse_args()
+
+
+def upload_to_mapbox(dir):
+
+    for file in os.listdir(dir):
+        output_file_name = file.split(".")[0]
+        valid_tileset_id = output_file_name.replace(" ", "_")
+
+        service = Uploader()
+        with open(os.path.join(os.path.abspath(dir), file), "rb") as src:
+            upload_resp = service.upload(
+                src,
+                tileset=valid_tileset_id
+                if len(valid_tileset_id) == 32
+                else valid_tileset_id[0:32],
+            )
+
+    return upload_resp
+
+
+def main(data_dir):
+    upload_to_mapbox(data_dir)
+
+
+if __name__ == "__main__":
+    args = get_args()
+    main(args.dir)
