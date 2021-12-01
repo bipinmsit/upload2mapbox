415e6f90ee945b95a2696aee10e2cf4d4bd70690
Bipin Kumar <bipin.kumar@laminaar.in>
2021-12-01 14:19:13 +0530

    sequential & parallel scripts created

---
+++ final_parallel_processing.py
@@ -0,0 +1,56 @@
+import multiprocessing as mp
+from mapbox import Uploader
+import argparse
+import os
+
+"""
+Script to upload mapbox supported data as tilesets (Parallel Processing)
+Tileset id are only first 14 char length
+
+Author: Bipin Kumar
+Company: Laminaar Aviation Infotech India Pvt. Ltd.
+Date: 17 Nov 2021
+"""
+
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        description="Upload mapbox supported data to mapbox"
+    )
+    parser.add_argument("--dir", type=str, help="Directory to upload to mapbox")
+
+    return parser.parse_args()
+
+
+def upload_to_mapbox(file_to_upload):
+    output_file_name = os.path.basename(file_to_upload).split(".")[0]
+    valid_tileset_name = output_file_name.replace(" ", "_")
+    valid_tileset_id = valid_tileset_name[0:14]
+
+    service = Uploader()
+    with open(file_to_upload, "rb") as src:
+        upload_resp = service.upload(
+            src,
+            name=valid_tileset_name
+            if len(valid_tileset_name) == 32
+            else valid_tileset_name[0:32],
+            tileset=valid_tileset_id,
+        )
+
+    return upload_resp
+
+
+def main(data_dir):
+    pool = mp.Pool(mp.cpu_count())
+    pool.map(
+        upload_to_mapbox,
+        [
+            os.path.join(os.path.abspath(data_dir), file)
+            for file in os.listdir(data_dir)
+        ],
+    )
+
+
+if __name__ == "__main__":
+    args = get_args()
+    main(args.dir)
---
+++ final_sequential_processing.py
@@ -0,0 +1,52 @@
+from mapbox import Uploader
+import argparse
+import os
+
+"""
+Script to upload mapbox supported data as tilesets (Sequential)
+Tileset id are only first 14 char length
+
+Author: Bipin Kumar
+Company: Laminaar Aviation Infotech India Pvt. Ltd.
+Date: 17 Nov 2021
+"""
+
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        description="Upload mapbox supported data to mapbox"
+    )
+    parser.add_argument("--dir", type=str, help="Directory to upload to mapbox")
+
+    return parser.parse_args()
+
+
+def upload_to_mapbox(upload_dir):
+
+    for file in os.listdir(upload_dir):
+        output_file_name = file.split(".")[0]
+        valid_tileset_name = output_file_name.replace(" ", "_")
+        valid_tileset_id = valid_tileset_name[0:14]
+
+        print("{} is uploading".format(valid_tileset_name))
+
+        service = Uploader()
+        with open(os.path.join(os.path.abspath(upload_dir), file), "rb") as src:
+            upload_resp = service.upload(
+                src,
+                name=valid_tileset_name
+                if len(valid_tileset_name) == 32
+                else valid_tileset_name[0:32],
+                tileset=valid_tileset_id,
+            )
+
+    return upload_resp
+
+
+def main(data_dir):
+    upload_to_mapbox(data_dir)
+
+
+if __name__ == "__main__":
+    args = get_args()
+    main(args.dir)
